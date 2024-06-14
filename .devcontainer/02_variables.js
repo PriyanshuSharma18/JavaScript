3d77ba3ed0f5cf91eb63d6e598ff42ea0fc5da3b
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-06-14 02:21:06 +0000

    DATA TYPES AND VARIABLES

---
+++ .devcontainer/02_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="tonystark@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2   //not allowed
+accountEmail="rdj@gmail.com"
+accountPassword="123456789"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+prefer not to use var because of issue in block
+*/
---
+++ .devcontainer/03_datatypes.js
@@ -0,0 +1,31 @@
+//"use strict";  //treat all Js as newer version
+//lert("hello"alert(3+3) we are using nodejs not browser
+
+console.log(3+9)
+
+console.log("SRK")
+
+//Datatypes
+let name="srk"
+let age=21
+let isLoggedIn=true
+
+
+
+/*
+int
+string
+boolean,BigInt
+null=>standalone value ,empty value,can be assigned
+undefined
+symbol
+
+
+object
+
+
+*/
+console.log(typeof"srk")
+console.log(typeof 123)
+console.log(typeof null)
+console.log(typeof undefined)
